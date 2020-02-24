1ed9f561e1e676b988f754ba2105762c11e6e54e
Aleksandar Kocic <25288526+kocic11@users.noreply.github.com>
2020-02-24 14:58:49 -0500

    Updated

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
+import argparse
 import json
-import requests
+import os
 import sys
 from datetime import date
+import requests
 
 result = json.loads(
   """
@@ -13,7 +15,9 @@ result = json.loads(
   """
 )
 
-def __getEnv(config):
+def __getEnv(env):
+  with open(env) as f:
+    config = json.load(f)
   user = config.get("user")
   password = config.get("password")
   id_tenant_name = config.get("id-tenant-name")
@@ -25,7 +29,28 @@ def __getEnv(config):
   }
   return user, password, id_tenant_name, jaas_uri, auth, headers
 
-def scale(config, jcsinstance, hosts, shape):
+def startstop(args, command):
+  """Return the result of the JCS instance start/stop operation."""
+  global result
+  data = json.loads(
+    """
+    {
+    "allServiceHosts" : true
+    } 
+    """
+  )
+
+  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(args.env) 
+
+  try:
+    uri = jaas_uri + "/instancemgmt/" + id_tenant_name + "/services/jaas/instances/" + args.instance + "/hosts/" + command
+    result = requests.post(uri, auth=auth, headers=headers, data=json.dumps(data)).json()
+  except:
+    result["details"]["message"] = "Unexpected error: " + str(sys.exc_info()[0])
+    pass
+  return result
+
+def scale(args):
   """Return the result of the JCS instance scale up/down operation."""
   global result
   data = json.loads(
@@ -42,12 +67,12 @@ def scale(config, jcsinstance, hosts, shape):
     """
   )
   
-  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(config)
+  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(args.env)
   
   try:
-    uri = jaas_uri + "/instancemgmt/" + id_tenant_name + "/services/jaas/instances/" + jcsinstance + "/hosts/scale"
-    data["components"]["WLS"]["hosts"] = hosts.split(",")
-    data["components"]["WLS"]["shape"] = shape
+    uri = jaas_uri + "/instancemgmt/" + id_tenant_name + "/services/jaas/instances/" + args.instance + "/hosts/scale"
+    data["components"]["WLS"]["hosts"] = args.hosts.split(",")
+    data["components"]["WLS"]["shape"] = args.shape
     
     result = requests.post(uri, auth=auth, headers=headers, data=json.dumps(data)).json()
   except:
@@ -55,38 +80,56 @@ def scale(config, jcsinstance, hosts, shape):
     pass
   return result
 
-def startstop(config, jcsinstance, command):
-  """Return the result of the JCS instance start/stop operation."""
-  global result
-  data = json.loads(
-    """
-    {
-    "allServiceHosts" : true
-    } 
-    """
-  )
+def start(args):
+  """Return the result of the JCS instance start operation."""
+  return startstop(args, "start")
 
-  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(config) 
+def stop(args):
+  """Return the result of the JCS instance stop operation."""
+  return startstop(args, "stop")
 
-  try:
-    uri = jaas_uri + "/instancemgmt/" + id_tenant_name + "/services/jaas/instances/" + jcsinstance + "/hosts/" + command
-    result = requests.post(uri, auth=auth, headers=headers, data=json.dumps(data)).json()
-  except:
-    result["details"]["message"] = "Unexpected error: " + str(sys.exc_info()[0])
-    pass
-  return result
 
-def activity(config, jcsinstance, fromStartDate = None):
+def activity(args):
   """Return the the JCS instance operations activity logs."""
   global result
-  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(config)
+  user, password, id_tenant_name, jaas_uri, auth, headers = __getEnv(args.env)
 
   try:
-    if fromStartDate == None or fromStartDate == '':
-      fromStartDate = date.today().strftime("%Y-%m-%d")
-    uri = jaas_uri + "/activitylog/" + id_tenant_name + "/filter?serviceName=" + jcsinstance + "&fromStartDate=" + fromStartDate
+    uri = jaas_uri + "/activitylog/" + id_tenant_name + "/filter?serviceName=" + args.instance + "&fromStartDate=" + args.fromStartDate
     result = requests.get(uri, auth=auth, headers=headers).json()
   except:
     result["details"]["message"] = "Unexpected error: " + str(sys.exc_info()[0])
     pass
   return result
+
+def print_usage(args):
+  global parser
+  parser.print_usage()
+  exit()
+
+def main():
+  global parser
+  parser.add_argument("env", help="JSON file with environment variables")
+  parser.add_argument("instance", help="JCS instance name")
+  parser.set_defaults(func=print_usage)
+  subparsers = parser.add_subparsers(help='sub-command help')
+  parser_scale = subparsers.add_parser("scale", help="Scale JCS instance")
+  parser_scale.add_argument("scale", help="Scale JCS instance", action='store_true')
+  parser_scale.add_argument("hosts", help="comma separated list of JCS instance hosts")
+  parser_scale.add_argument("shape", help="JCS instance shape")
+  parser_scale.set_defaults(func=scale)
+  parser_start = subparsers.add_parser("start", help="Start JCS instance")
+  parser_start.add_argument("start", action='store_true')
+  parser_start.set_defaults(func=start)
+  parser_stop = subparsers.add_parser("stop", help="Stop JCS instance")
+  parser_stop.add_argument("stop", action='store_true')
+  parser_stop.set_defaults(func=stop)
+  parser_logs = subparsers.add_parser("logs", help="Show JCS instance activity logs")
+  parser_logs.add_argument("logs", action='store_true')
+  parser_logs.add_argument("-d", "--fromStartDate", help="filter activities based on start date (YYYY-MM-DD)", default=date.today().strftime("%Y-%m-%d"))
+  parser_logs.set_defaults(func=activity)
+  args = parser.parse_args()
+  return args.func(args)
+
+parser = argparse.ArgumentParser()
+print(main())
--- run.py
+++ run.py
@@ -1,19 +1,39 @@
 import json
 import os
 import sys
+import argparse
+
 from functions import *
 
-lenArgs = len(sys.argv)
-if (lenArgs < 2) :
-    print("Usage: python", sys.argv[0], "env.json")
-    exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("env", help="JSON file with environment variables")
+parser.add_argument("instance", help="JCS instance name")
+# parser.add_argument("--start", help="Start JCS instance", action='store_true')
+# parser.add_argument("--stop", help="Stop JCS instance", action='store_true')
+# parser.add_argument("--scale", help="Scale JCS instance", action='store_true')
+subparsers = parser.add_subparsers(help='sub-command help')
+parser_scale = subparsers.add_parser("scale", help="Scale JCS instance")
+# parser_scale.add_argument("scale", help="Scale JCS instance", action='store_true')
+parser_scale.add_argument("hosts", help="comma separated list of JCS instance hosts")
+parser_scale.add_argument("shape", help="JCS instance shape")
+parser_start = subparsers.add_parser("start", help="Start JCS instance")
+# parser_start.add_argument("start", action='store_true')
+parser_stop = subparsers.add_parser("stop", help="Stop JCS instance")
+# parser_stop.add_argument("stop", action='store_true')
+parser_logs = subparsers.add_parser("logs", help="Show JCS instance activity logs")
+# parser_logs.add_argument("logs", action='store_true')
+args = parser.parse_args()
 
-with open(sys.argv[1]) as f:
+with open(args.env) as f:
   config = json.load(f)
 
-print("Scale up/down")
-print(scale(config, "testjcs", "testjcs-wls-1", "VM.Standard2.1"))
-print("Start/Stop")
-print(startstop(config, "testjcs", "start"))
-print("Activity Logs")
-print(activity(config, "testjcs"))
+if args.scale:
+    print("Scale up/down")
+    print(scale(config, args.instance, args.hosts, args.shape))
+
+if args.stop or args.start:
+  print("Start/Stop")
+  print(startstop(config, args.instance, args.command))
+if args.logs:
+  print("Activity Logs")
+  print(activity(config, args.instance))
